284dd79c4cce3e2cea8fba8387056ddafd3448fb
sohyp3 <0xshb@protonmail.com>
2025-01-12 23:50:33 +0300

    insta + maps

--- src/lib/components/SideBar.svelte
+++ src/lib/components/SideBar.svelte
@@ -14,8 +14,6 @@
 
 	let categoriesList = [];
 	categories.subscribe((value) => (categoriesList = value));
-
-
 </script>
 
 <aside class="flex flex-col gap-3 w-48 h-full bg-primary">
@@ -34,4 +32,19 @@
 			</button>
 		</div>
 	{/each}
+	<div class="flex gap-3 justify-evenly w-full">
+		<a
+			href="https://www.instagram.com/blounge.1/"
+			target="_blank"
+			class="flex gap-1 justify-center items-center w-12 text-white transition-shadow duration-200 hover:shadow-2xl"
+			><img src="/icons/insta.svg" alt="insta" /></a
+		>
+
+		<a
+			href="https://maps.app.goo.gl/1UmT5ZSxxo3hLKBPA"
+			target="_blank"
+			class="flex gap-1 justify-center items-center w-12 text-white transition-shadow duration-200 hover:shadow-2xl"
+			><img src="/icons/maps.svg" alt="insta" /></a
+		>
+	</div>
 </aside>
